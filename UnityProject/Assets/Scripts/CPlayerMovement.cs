3d58e4dd2c8b6b7a03e48ed2026c5914658476a9
Shamim Akhtar <shamim.akhtar@gmail.com>
2022-01-13 12:49:51 +0800

    Added jump, crouch, attack1, attack2 and reload animations.

--- UnityProject/Assets/Scripts/CPlayerMovement.cs
+++ UnityProject/Assets/Scripts/CPlayerMovement.cs
@@ -12,10 +12,16 @@ public class CPlayerMovement : MonoBehaviour
   public float mWalkSpeed = 1.5f;
   public float mRotationSpeed = 50.0f;
 
+  public AudioSource mAudioSource;
+  public AudioClip mAudioClip_Jump;
+  public AudioClip mAudioClip_Attack1;
+  public AudioClip mAudioClip_Reload;
+
 
   // Update is called once per frame
   void Update()
   {
+    HandleInputs();
     // The following code takes input and uses it for
     // the walk/run movement.
     // Since we are using a shift button key to 
@@ -51,5 +57,66 @@ public class CPlayerMovement : MonoBehaviour
 
     mAnimator.SetFloat("PosX", 0);
     mAnimator.SetFloat("PosZ", vInput * speed / (2.0f * mWalkSpeed));
+  }
+
+  void HandleInputs()
+  {
+    if(Input.GetKeyDown(KeyCode.Space))
+    {
+      Jump();
+    }
+    if(Input.GetKeyDown(KeyCode.X))
+    {
+      StartAttack1();
+    }
+    if (Input.GetKeyUp(KeyCode.X))
+    {
+      StopAttack1();
+    }
+    if (Input.GetKeyDown(KeyCode.C))
+    {
+      StartAttack2();
+    }
+    if (Input.GetKeyUp(KeyCode.C))
+    {
+      StopAttack2();
+    }
+    if (Input.GetKeyDown(KeyCode.R))
+    {
+      Reload();
+    }
+  }
+
+  void Jump()
+  {
+    mAnimator.SetTrigger("Jump");
+
+  }
+
+  void StartAttack1()
+  {
+    mAnimator.SetBool("Attack1", true);
+    //mAudioSource.PlayOneShot(mAudioClip_Attack1);
+  }
+
+  void StopAttack1()
+  {
+    mAnimator.SetBool("Attack1", false);
+  }
+
+  void StartAttack2()
+  {
+    mAnimator.SetBool("Attack2", true);
+    //mAudioSource.PlayOneShot(mAudioClip_Attack1);
+  }
+
+  void StopAttack2()
+  {
+    mAnimator.SetBool("Attack2", false);
+  }
+
+  void Reload()
+  {
+    mAnimator.SetTrigger("Reload");
   }
 }
